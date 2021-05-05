dff1a8aca9cc873a3e5fdd5ca138ffd1c8aac504
Asim <asim005011997@gmail.com>
2021-05-05 17:11:37 +0530

    Container updated

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,16 @@
+import { connet } from 'react-redux'
 import Home from '../Components/Home'
+import { addToCart } from '../Services/Actions/action'
 
-export default Home
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+    addToCartHandler: data => dispatch(addToCart(data))
+})
+
+
+export default connet(mapDispatchToProps, mapStateToProps)(Home)
+
+// export default Home
